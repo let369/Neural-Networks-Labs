6545203f4a225e4cac380b762f1e8e777afb22d1
TzafosPanagiotis <ptzafos@gmail.com>
2017-03-01 12:04:33 +0100

    Neural lab2

--- Lab 0/src/noisyifneuron.m
+++ Lab 0/src/noisyifneuron.m
@@ -17,8 +17,11 @@ Rin = Rin_vector(2); %assigning starting values (middle) for the constants
 theta = theta_vector(2);
 
 legendInfo = cell(1,3); %initialization of legend cell for the figures
-i=1; %initialization of i that counts the legends that we are gonna have in our figure
-for tau=tau_vector %sweep of tau value while keeping the other 2 variables constant
+i=1; %initialization of i that counts the legends 
+     %that we are gonna have in our figure
+
+for tau=tau_vector %sweep of tau value while keeping the 
+		   %other 2 variables constant
 	for n=2:nstep
 		v(n)=v(n-1) + dt*(-v(n-1)/tau + Rin*IO(n)/tau);
 		if (v(n) > theta)
@@ -26,10 +29,14 @@ for tau=tau_vector %sweep of tau value while keeping the other 2 variables const
 			tspike = [tspike t(n)];
 		end
 	end
-	figure(figHandleTau) %use of figure command so that our plots for the diffeerent sweeps be in different figures
-	legendInfo{i} = ['Tau = ' num2str(tau) ' Mean Spike interval = ' num2str(mean(isi(tspike)))]; %add the values of the sweeping var as legend + the mean of interval spikes
+	figure(figHandleTau) %use of figure command so that our plots for
+                  	     %the diffeerent sweeps be in different figures
+	legendInfo{i} = ['Tau = ' num2str(tau) ... %add the values of
+	' Mean Spike interval = ' num2str(mean(isi(tspike)))]; 
+ 	%the sweeping var as legend + the mean of interval spikes
 	tspike = []; %initialization of tspike table for the next sweep
-	hold all %hold all command so that all plots of this iteration are represented at the same figure
+	hold all %hold all command so that all plots of this iteration 
+		 %are represented at the same figure
 	title('Parameter Sweeping for Tau');
 	xlabel('Time');
 	ylabel('Voltage');
@@ -51,7 +58,8 @@ for Rin=Rin_vector
 		 end
 	 end
 	 figure(figHandleRin)
-	 legendInfo{i} = ['Rin = ' num2str(Rin) ' Mean Spike interval = ' num2str(mean(isi(tspike)))];
+	 legendInfo{i} = ['Tau = ' num2str(tau) ...
+	 ' Mean Spike interval = ' num2str(mean(isi(tspike)))]; 
 	 tspike = [];
 	 hold all
 	 title('Parameter Sweeping for Rin');
@@ -60,7 +68,6 @@ for Rin=Rin_vector
 	 plot(t,v)
 	 i=i+1;
 end
- 
 legend(legendInfo)
 
 Rin = Rin_vector(2);
@@ -77,7 +84,8 @@ for theta=theta_vector
 		 end
 	 end
 	 figure(figHandleTheta)
-	 legendInfo{i} = ['Theta = ' num2str(theta) ' Mean Spike interval = ' num2str(mean(isi(tspike)))];
+	 legendInfo{i} = ['Theta = ' num2str(theta) ...
+	 ' Mean Spike interval = ' num2str(mean(isi(tspike)))];
 	 tspike = [];
 	 hold all
 	 title('Parameter Sweeping for Theta');
